3c962ba375bce59ab418f1cfd8655d6e6ec9433d
chenyz-10.21.52.18 <ahchen-yz@outlook.com>
2023-08-13 17:20:10 +0800

    hir-stateop

---
+++ compiler/include/hir-stateop.hpp
@@ -0,0 +1,14 @@
+#pragme once 
+
+#include "highLv-ir.hpp"
+#include "hir-common-pass.hpp"
+
+namespace HIR
+{
+    void split_stateptr_branch(Element &ele);
+    void replace_vector_ops(Element& ele);
+    void replace_map_ops(Element& ele);
+    void replace_fixsized_array_ops(Element& ele);
+
+    void replace_regular_struct_access(Function& func);
+} 
